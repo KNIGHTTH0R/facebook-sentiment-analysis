b660dc68f0dbaf6f4c09ff42a5d9ba24cf8266a9
knightthor <sugihlia@gmail.com>
2017-11-04 05:39:01 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
 // Configure your Datumbox API Key. Get yours at http://www.datumbox.com/apikeys/view/
-define('DATUMBOX_API_KEY', '');
+define('a1c3f44e96a51aa6a2327412ad63608c', '');
 
 // Configure your Facebook App and get your credentials from https://developers.facebook.com/
-define('FACEBOOK_APP_ID', '');
-define('FACEBOOK_APP_SECRET', ''); 
+define('790826791020957', '');
+define('e462d18a18e596d8ebb3c73159acee82', ''); 
